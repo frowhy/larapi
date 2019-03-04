baec365cb11fe5ef3a36c9f7b2ee957f64556cd4
frowhy <frowhy@gmail.com>
2019-03-04 09:34:58 +0800

    重命名方法 b -> is_true
    Signed-off-by: frowhy <frowhy@gmail.com>

--- Modules/Core/helpers.php
+++ Modules/Core/helpers.php
@@ -553,9 +553,13 @@ if (!function_exists('asciiEncode')) {
 /**
  * Get the boolean value of a variable
  */
-if (!function_exists('b')) {
-    function b($val): bool
+if (!function_exists('is_true')) {
+    function is_true($val): bool
     {
-        return $val instanceof \Modules\Core\Contracts\Support\Boolable ? $val->toBool() : boolval($val);
+        return $val instanceof \Modules\Core\Contracts\Support\Boolable
+            ? $val->toBool()
+            : (is_string($val)
+                ? filter_var($val, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE)
+                : boolval($val));
     }
 }
