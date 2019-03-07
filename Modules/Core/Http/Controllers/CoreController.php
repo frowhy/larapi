f9622c88205cd4bc44efe8492c246f98ab80b2a3
frowhy <frowhy@gmail.com>
2019-03-07 15:30:19 +0800

    移除测试代码
    Signed-off-by: frowhy <frowhy@gmail.com>

--- Modules/Core/Http/Controllers/CoreController.php
+++ Modules/Core/Http/Controllers/CoreController.php
@@ -4,7 +4,6 @@ namespace Modules\Core\Http\Controllers;
 
 use Illuminate\Routing\Controller;
 use Module;
-use Modules\Core\Services\CourseService;
 use Modules\Core\Supports\Response;
 
 class CoreController extends Controller
@@ -12,12 +11,10 @@ class CoreController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @param \Modules\Core\Services\CourseService $service
      * @return \Modules\Core\Supports\Response
      */
-    public function index(CourseService $service)
+    public function index()
     {
-        dd($service);
         $name = Module::find('core')->name;
         $requirements = collect(Module::findRequirements('core'));
         $requirements = $requirements->map(function ($item) {
