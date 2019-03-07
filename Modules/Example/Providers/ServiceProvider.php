a8db38bde222ca4c6100ecd3109edd6ca091c5da
frowhy <frowhy@gmail.com>
2019-03-07 12:10:00 +0800

    添加示例模块
    Signed-off-by: frowhy <frowhy@gmail.com>

---
+++ Modules/Example/Providers/ServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Example\Providers;
+
+
+use Illuminate\Support\ServiceProvider as ServiceProviderAlias;
+
+class ServiceProvider extends ServiceProviderAlias
+{
+    /**
+     * 绑定服务接口
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app->bind(\Modules\Example\Services\ExampleService::class, \Modules\Example\Services\ExampleServiceI::class);
+        //
+
+    }
+}
